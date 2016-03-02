948ff473e671b86ddafb7b75a7c9c9c5970fdd82
Joakim <rtuu41@gmail.com>
2016-03-03 00:38:11 +0100

    QuestionCreate start

---
+++ viewVerktyg/QuestionCreator.java
@@ -0,0 +1,24 @@
+package viewVerktyg;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class QuestionCreator {
+	Button title, save, addOption;
+	TextField txtQuestion;
+	
+	public void generateOptionField (){
+		addOption.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+	
+}
