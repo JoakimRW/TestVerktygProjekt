85c63781ba0f9a34232b368e463b69414e3cd4aa
Joakim <rtuu41@gmail.com>
2016-03-03 13:09:10 +0100

    Form Created Form class

--- viewVerktyg/form.java
+++ viewVerktyg/form.java
@@ -2,7 +2,36 @@ package viewVerktyg;
 
 import java.util.ArrayList;
 
-public class form {
+public class Form {
 	private String query,answer;
 	private ArrayList<String> options;
+	private int corrAns;
+	
+	public Form(){
+		
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String getQuery() {
+		return query;
+	}
+	public void setQuery(String query) {
+		this.query = query;
+	}
+	public int getCorrAns() {
+		return corrAns;
+	}
+	public void setCorrAns(int corrAns) {
+		this.corrAns = corrAns;
+	}
+	public ArrayList<String> getOptions() {
+		return options;
+	}
+	public void setOptions(ArrayList<String> options) {
+		this.options = options;
+	}
 }
